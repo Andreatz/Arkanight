16d8a06cc00418b2666d1a8aec86db0b95068e5d
Andreatz <53533849+Andreatz@users.noreply.github.com>
2026-04-30 21:19:42 +0200

    Fix import statement in supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-mport { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 let _client: SupabaseClient | null = null;
 
